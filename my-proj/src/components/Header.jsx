bc9f274f6d5f58e91b3ab67a18cb22e7ecb0a8c6
MaximTurchin <maxas1488@gmail.com>
2025-08-28 15:59:35 +0300

    fix font

--- my-proj/src/components/Header.jsx
+++ my-proj/src/components/Header.jsx
@@ -13,8 +13,9 @@ function HeaderContent(){
        <div className="flex flex-col md:flex-row w-full">
         <div className="text-[2.4rem] font-bold leading-[3.2rem] w-full text-center ">Ricardo Cooper</div>
         <div className="flex gap-5">
-            <button className="rounded-md bg-white px-5 py-3 text-[1.4rem] hover:bg-gray-300 cursor-pointer">Message</button>
-            <button className="rounded-md bg-white px-5 py-3 text-[1.4rem] hover:bg-gray-300 cursor-pointer">Call</button>
+            <button className="rounded-md bg-white px-5 py-3 text-[1.4rem] leading-[2rem] hover:bg-gray-300 cursor-pointer flex items-center gap-2">
+                <div className="size-[2rem] items-center justify-center"><img src="./src/assets/icons/mail.svg" alt="" className="w-full h-full"/></div><div>Message</div></button>
+            <button className="rounded-md bg-white px-5 py-3 text-[1.4rem] leading-[2rem] hover:bg-gray-300 cursor-pointer flex items-center gap-2"><div className="size-[2rem] items-center justify-center"><img src="./src/assets/icons/phone-left.svg" alt="" className="w-full h-full"/></div><div>Call</div></button>
         </div>
        </div>
        
--- my-proj/src/components/ModalImage.jsx
+++ my-proj/src/components/ModalImage.jsx
@@ -21,17 +21,17 @@ export default function ModalImage({closeModel, image}){
         <div className="xl:w-[69.3rem] xl:h-[85.2rem] flex flex-col items-center bg-white rounded-2xl w-[90%] px-10 py-5" onClick={(e)=>{e.stopPropagation()}}>
             <img src={image.largeImage} alt="" className="rounded-4xl w-[40.5rem] mb-20"/>
             <div className="text-[#374151] w-full mb-10"> 
-                <label className="text-[1.4rem] font-medium">Comment</label>
+                <label className="text-[1.4rem] font-medium leading-[1.4rem]">Comment</label>
                 <textarea className="w-full h-[10.5rem] outline-none border-2 border-gray-500/50 rounded-xl text-[1.4rem]" name="comment-textarea" id="comment-textarea" value={comment} onChange={(e)=> setComment(e.target.value)}></textarea>
-                <label htmlFor="" className="text-[1.4rem]">Write a few sentences about the photo.</label>
+                <label htmlFor="" className="text-[1.4rem] leading-[2rem]">Write a few sentences about the photo.</label>
             </div>
             {isFail==='fail' && <p className="text-[1.4rem] text-red-500">Извините комментарий не сохранился</p>}
             {isFail==='successed' && <p className="text-[1.4rem] text-green-500">Отправлено</p>}
             <button onClick={addCommentApi} className="p-1 text-[1.4rem] mb-5 bg-indigo-600 w-[6.7rem] h-[3.8rem] rounded-xl hover:bg-indigo-900 cursor-pointer">Save</button>
             <div className="text-black flex flex-col items-start w-full">
-                <div className="mb-5">Comments</div>
+                <div className="mb-5 text-[1.4rem] leading-[2rem]">Comments</div>
                 <ul>
-                    {image.comments.map((comment) => <li key={comment.id} className="mb-2">#{comment.id} {comment.author} {comment.text}</li>)}
+                    {image.comments.map((comment) => <li key={comment.id} className="mb-2 text-[1.4rem] leading-[2rem]">#{comment.id} {comment.author} {comment.text}</li>)}
                 </ul>
             </div>
             
